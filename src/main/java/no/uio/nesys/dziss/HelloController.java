6c0fc20718460f1ba6677f69c28c9d856ac829d4
dmitrd <dmitrd@dmitris-macbook-pro.home>
2021-08-10 19:09:40 +0200

    @RequestMapping("hello")     public String sayHello(){         return ("Hello, SpringBoot on Wildfly");     }

--- src/main/java/no/uio/nesys/dziss/HelloController.java
+++ src/main/java/no/uio/nesys/dziss/HelloController.java
@@ -1,19 +1,20 @@
 package no.uio.nesys.dziss;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+	/*
+	 * @GetMapping("/") public String index() { return
+	 * "Greetings from Spring Boot!"; }
+	 */
 	
-	@GetMapping("/xxx")
-	public String xxx() {
-		return "Greetings from XXX";
-	}
+	@RequestMapping("hello")
+    public String sayHello(){
+        return ("Hello, SpringBoot on Wildfly");
+    }
 
 }
