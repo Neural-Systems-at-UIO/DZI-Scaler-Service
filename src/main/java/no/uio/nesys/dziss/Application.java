fa7d463ce0c8c9d65dc86e1be08dff6aeae9d926
dmitrd <dmitrd@dmitris-macbook-pro.home>
2021-08-10 21:32:06 +0200

    @Configuration @ComponentScan @EnableAutoConfiguration

--- src/main/java/no/uio/nesys/dziss/Application.java
+++ src/main/java/no/uio/nesys/dziss/Application.java
@@ -4,10 +4,16 @@ import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
