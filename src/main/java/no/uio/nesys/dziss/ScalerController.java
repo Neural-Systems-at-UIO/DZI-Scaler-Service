550e9f0dbd9cb05c2789228369325edd42aced22
dmitrd <dmitrd@dmitris-macbook-pro.home>
2021-08-12 20:22:28 +0200

    v.1

--- src/main/java/no/uio/nesys/dziss/ScalerController.java
+++ src/main/java/no/uio/nesys/dziss/ScalerController.java
@@ -23,61 +23,83 @@ import no.uio.nesys.pyramidio.DeepZoomImageReaderUrl;
 
 @RestController
 public class ScalerController {
-	
-	private static final Logger logger = Logger.getLogger(
-			ScalerController.class.getName());
-	
+
+	private static final Logger logger = Logger.getLogger(ScalerController.class.getName());
+
 	@GetMapping("/")
 	public String index() {
 		return "DZI Scaler Service v.1";
 	}
 
-	@RequestMapping("/scaler")
+	private ResponseEntity<InputStreamResource> getResponseEntity(BufferedImage bi, HttpHeaders headers, String format)
+			throws IOException {
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(bi, format, baos);
+
+		byte[] imageInByte = baos.toByteArray();
+
+		InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(imageInByte));
+
+		MediaType mt;
+		switch (format.toLowerCase()) {
+		case "png":
+			mt = new MediaType(MediaType.parseMediaType(MediaType.IMAGE_PNG_VALUE));
+			break;
+		case "jpg":
+			mt = new MediaType(MediaType.parseMediaType(MediaType.IMAGE_JPEG_VALUE));
+			break;
+		case "jpeg":
+			mt = new MediaType(MediaType.parseMediaType(MediaType.IMAGE_JPEG_VALUE));
+			break;
+		default:
+			return ResponseEntity.badRequest().body(null);
+		}
+
+		return ResponseEntity.ok().headers(headers).contentLength(imageInByte.length).contentType(mt).body(resource);
+	}
+
+	@RequestMapping("/scale")
+	public ResponseEntity<InputStreamResource> scaler(@RequestParam String dzi, @RequestParam double scale,
+			@RequestParam(defaultValue = "png") String format) {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("content-disposition", "inline;filename=" + "scaled_" + scale + "." + format);
+
+		try {
+
+			DeepZoomImageReaderUrl reader = new DeepZoomImageReaderUrl(new URL(dzi));
+			BufferedImage scaled = reader.getWholeImage(scale);
+
+			return getResponseEntity(scaled, headers, format);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.badRequest().body(null);
+
+		}
+
+	}
+
+	@RequestMapping("/region")
 	public ResponseEntity<InputStreamResource> scaler(@RequestParam String dzi, @RequestParam int x,
 			@RequestParam int y, @RequestParam int width, @RequestParam int height,
 			@RequestParam(defaultValue = "1") double scale, @RequestParam(defaultValue = "png") String format) {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("content-disposition",
+				"inline;filename=" + x + "_" + y + "_" + width + "_" + height + "_" + scale + "." + format);
 
 		try {
 			DeepZoomImageReaderUrl reader = new DeepZoomImageReaderUrl(new URL(dzi));
 			BufferedImage region = reader.getSubImage(new Rectangle(x, y, width, height), scale);
 
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(region, format, baos);
+			return getResponseEntity(region, headers, format);
 
-			byte[] imageInByte = baos.toByteArray();
-
-			HttpHeaders headers = new HttpHeaders();
-			headers.add("content-disposition",
-					"inline;filename=" + x + "_" + y + "_" + width + "_" + height + "_" + scale + "." + format);
-
-			InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(imageInByte));
-			
-			logger.info("Image size: " + imageInByte.length);;
-
-			MediaType mt;
-			switch (format.toLowerCase()) {
-			case "png":
-				mt = new MediaType(MediaType.parseMediaType(MediaType.IMAGE_PNG_VALUE));
-				break;
-			case "jpg":
-				mt = new MediaType(MediaType.parseMediaType(MediaType.IMAGE_JPEG_VALUE));
-				break;
-			case "jpeg":
-				mt = new MediaType(MediaType.parseMediaType(MediaType.IMAGE_JPEG_VALUE));
-				break;
-			default:
-				return ResponseEntity.badRequest().body(null);
-			}
-
-			return ResponseEntity.ok().headers(headers).contentLength(imageInByte.length).contentType(mt)
-					.body(resource);
-
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.badRequest().body(null);
 		}
-
-		// https://dziss.apps-dev.hbp.eu/index.html?dzi=https://object.cscs.ch/v1/AUTH_08c08f9f119744cbbf77e216988da3eb/imgsvc-890b727c-f3b5-624f-028a-4f6d35acce18/D1R_P70_F_C60_s001.tif/D1R_P70_F_C60_s001.dzi&x=100&y=150&width=200&height=250&scale=0.5&format=png
 
 	}
 
